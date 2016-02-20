d750b44930b28fc208936f4ff2db57130bbf3fff
Xisco Sastre <xiscosastre@gmail.com>
2016-02-20 20:55:14 +0100

    web changes

--- app/src/main/java/com/pragmapure/flyevents/webActivity.java
+++ app/src/main/java/com/pragmapure/flyevents/webActivity.java
@@ -3,10 +3,12 @@ package com.pragmapure.flyevents;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import com.pragmapure.flyevents.classes.Photo;
 
@@ -26,12 +28,34 @@ public class webActivity extends AppCompatActivity {
         String lat = prefs.getString(Constants.GPS_LAT_KEY, null);
         String longi = prefs.getString(Constants.GPS_LONG_KEY, null);
 
-        String url = Constants.SERVER_URL;
+        String url = Constants.HOME_URL;
         if (prefs.getBoolean(Constants.EVENTS_NOTIFICATION, false)) {
             prefs.edit().putBoolean(Constants.EVENTS_NOTIFICATION, false).apply();
             url = Constants.WEB_EVENTS_URL;
         }
         myWebView.loadUrl(url+"?imei="+imei+"&latitude="+lat+"&longitude="+longi);
+    }
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
     }
 
 }
