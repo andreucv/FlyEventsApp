08f0ee692e128a8c516c82fc24a67c33b8e86424
Xisco Sastre <xiscosastre@gmail.com>
2016-02-20 01:43:30 +0100

    gps and internet permissions

---
+++ app/src/main/java/com/pragmapure/flyevents/webActivity.java
@@ -0,0 +1,26 @@
+package com.pragmapure.flyevents;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class webActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://in.mundiaguabalear.com");
+    }
+
+}
